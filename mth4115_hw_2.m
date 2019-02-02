dc59e50ebfc99844f7a8f234b140b9cc9f01bec5
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2019-02-02 00:01:15 -0500

    Updated Files

--- mth4115_hw_2.m
+++ mth4115_hw_2.m
@@ -1,10 +1,20 @@
 clear 
 
 
-function calc = newton_method()
-    calc=1;
+function calc = newton_method(initial_guess, epsilon)
+    calc = initial_guess - equ(initial_guess)/deri(initial_guess);
+    old_calc = 0;
+    while abs(old_calc - calc) > epsilon
+        old_calc = calc;
+        calc = calc - equ(calc)/deri(calc); 
+    end
 end
 
-function calc = secant_method()
-    calc=1;
+function calc = secant_method(a, b, epsilon)
+    calc = b - (equ(b)*(b-a))/(equ(b)-equ(a));
+    old_calc = 0;
+    while abs(old_calc - calc) > epsilon
+        old_calc = calc;
+        calc = = b - (equ(b)*(b-calc))/(equ(b)-equ(calc)) 
+    end
 end
