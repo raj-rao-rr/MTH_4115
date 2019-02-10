bbc666380caeb25bf9e1633b886f12a8b8240097
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2019-02-10 10:23:39 -0500

    Updated Files

--- mth4115_hw_5.m
+++ mth4115_hw_5.m
@@ -1,24 +1,35 @@
 clear 
 
-[fe, emf, cond] = stats(6);
+[fe, emf, cond] = stats(10);
 disp(fe)
-
+disp(emf)
+disp(cond)
 
 %  calculates the forward error, error magnification factor and conditional
 function [fe, emf, cond] = stats(n)
     [m,b] = spec_matrix(n);
     x = m\b;
-    x_a = transpose(repelem(1,6));
-    
+    x_a = transpose(repelem(1,n));
+ 
     fe = max(abs(x-x_a));
     emf = (norm(x-x_a)*norm(b)) / (norm(b-(m*x_a))*norm(x));
-    cond = norm(transpose(m))*norm(transpose(inv(m)));
+    cond = m_norm(m)*m_norm(inv(m));
 end
 
 %  finds the infinte norm of a given vector 
 function n = norm(v)
     n = max(abs(v));
 end 
+
+%  finds the infinte norm of a given matrix 
+function n = m_norm(m)
+    x = transpose(repelem(1,length(m)));
+    for i = 1:length(m)
+        val = sum(abs(m(i,:)));
+        x(i) = val;
+    end
+    n = max(x);
+end
 
 % creates the special matrix and the b defined in problem set 
 function [m,b] = spec_matrix(n)
