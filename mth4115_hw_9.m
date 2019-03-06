eb983878aef3ecbcde87d6d2dc60cd0a8f4fe52d
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2019-03-05 20:31:29 -0500

    Updated Files

--- mth4115_hw_9.m
+++ mth4115_hw_9.m
@@ -1,20 +1,21 @@
 clear 
 
-rand_matrix = randn(30, 30);
+rand_matrix = randn(30, 10);
 rand_vector = randn(30, 1);
 
 
-for i = 1:3 % iterates through to generate 3 random vectors to test residual condition 
-    di = randn(10, 1);
+% iterates through to generate 3 random vectors to test residual condition 
+for i = 1:3 
+    d = randn(10, 1);
     [x_hat, residual] = least_square(rand_matrix, rand_vector);	
-    sec_residual = norm((A* (x_hat+di))-b)^2;
+    % secondary residual based on ||A(x+d)-b||^2     
+    sec_residual = norm((rand_matrix * (x_hat+d))-rand_vector)^2;
     if sec_residual > residual
         disp('Condition holds for given vector d')
-        disp(d)
     end
 end
 
-
+% formulazines the least squares system to minimize errror
 function [x_hat, residual] = least_square(A, b)
 	x_hat = (transpose(A)*A)\transpose(A)*b;
 	residual = norm((A* x_hat)-b)^2;
