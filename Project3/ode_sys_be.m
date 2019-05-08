a8820bac628552175866a02e431dbcc41819db2e
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2019-05-07 21:33:57 -0400

    Updated Files

--- Project3/ode_sys_be.m
+++ Project3/ode_sys_be.m
@@ -6,7 +6,7 @@ function V = ode_sys_be(y0, t0, n, h, A)
 %     adjust for step size and count value from intial starting 
     for i = t0:h:((n-1)*h)+t0
 %         refrence the explicit trapezoid method for itterative step
-        new_A = (eye(size(A))-h)*A;
+        new_A = (eye(length(w_initial))-h*A);
         w_next = new_A\w_initial;
         w_initial = w_next;
     end
